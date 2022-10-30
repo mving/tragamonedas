fe0ae7b4ca38e5597294984b6177fd283c7e8a8b
Manuel <manuelvidal097@gmail.com>
2022-10-29 22:18:23 -0300

    first commit

---
+++ src/modelo/Maquina.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Maquina {
+	
+	private int numeroCasillas;
+	private float recaudacion;
+	private float recaudacionMinimo;
+	private float precioJugada;
+	private float credito;
+}
---
+++ src/modelo/Ticket.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Ticket {
+	
+	private String codigo;
+	private Float valor;
+	
+}
