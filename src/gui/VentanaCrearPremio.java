f7976c6489835a4d62421b72d407050637040ffb
MV_ing <36088504+mving@users.noreply.github.com>
2022-11-24 18:45:47 -0300

    Actualizar VentanaCrearPremio.java

--- src/gui/VentanaCrearPremio.java
+++ src/gui/VentanaCrearPremio.java
@@ -8,16 +8,22 @@ import modelo.Maquina;
 public class VentanaCrearPremio extends JDialog{
 	
 	private String[] frutas;
-	
+	private int cantidadCasillas, idMaquina;
+	private	Casino c;
+
 	public VentanaCrearPremio(VentanaPrincipal miVentanaPrincipal, boolean modal, Casino c) {
 		super(miVentanaPrincipal, modal);
 		setResizable(false);
+		setSize(300,300);
+		this.c = c;
+		this.idMaquina = idMaquina;
+		this.cantidadCasillas = c.cantidadCasillas(idMaquina);
 		iniciarComponentes();
 	}
 	
 	private void iniciarComponentes() {
 		frutas = Maquina.frutas;	//Mi ventana conoce a la maquina?
-		
+			
 	}
 	
 }
